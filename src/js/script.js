be204a49a0b7714aefd069899f3fb0a86356c4d8
Alexandr Sekerin <blazzed21@gmail.com>
2024-01-16 18:52:55 +0300

    chore: add command for building

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,19 @@
-window.addEventListener("DOMContentLoaded", () => {
-    const menu = document.querySelector(".menu");
-    const menuItem = document.querySelectorAll(".menu_item");
-    const hamburger = document.querySelector(".hamburger");
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.menu');
+  const menuItem = document.querySelectorAll('.menu_item');
+  const hamburger = document.querySelector('.hamburger');
 
-    hamburger.addEventListener("click", () => {
-      hamburger.classList.toggle("hamburger_active");
-      menu.classList.toggle("menu_active");
-    });
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger_active');
+    menu.classList.toggle('menu_active');
+  });
 
-    menuItem.forEach((item) => {
-      item.addEventListener("click", () => {
-        hamburger.classList.toggle("hamburger_active");
-        menu.classList.toggle("menu_active");
-      });
+  menuItem.forEach((item) => {
+    item.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('menu_active');
     });
+  });
 });
-
-
-
 
 
